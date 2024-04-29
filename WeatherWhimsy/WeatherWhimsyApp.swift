bb80b2dd3524ea1a11015831712c9100eeb5a856
FlorianTrecul <trecul.florian@gmail.com>
2024-04-29 16:22:10 +0200

    Initial Commit

---
+++ WeatherWhimsy/WeatherWhimsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWhimsyApp.swift
+//  WeatherWhimsy
+//
+//  Created by Florian Trecul on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWhimsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
